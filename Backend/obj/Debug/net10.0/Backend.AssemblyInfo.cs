c2c91ad26ded5cf6cc67faeecbad5a359a947698
kelomire <leonhuapayaanderson@gmail.com>
2026-07-07 16:31:43 -0300

    mejora 1

--- Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
+++ Backend/obj/Debug/net10.0/Backend.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Backend")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+801b3663acd51ee66011d85779dcdf4ae3a34d54")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1a3bfcaf9adf648443e4f0fb4ace92e18866d591")]
 [assembly: System.Reflection.AssemblyProductAttribute("Backend")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Backend")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
