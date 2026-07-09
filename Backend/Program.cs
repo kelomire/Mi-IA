f1c80b22ac5762fc829bba8ae6a2fb6610746f62
kelomire <leonhuapayaanderson@gmail.com>
2026-07-09 20:46:51 -0300

    mejora 2

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,16 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
 
+// Agregar servicios
 builder.Services.AddControllers();
-builder.Services.AddOpenApi();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
+// Configuración del pipeline
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthorization();
 
 app.MapControllers();
 
